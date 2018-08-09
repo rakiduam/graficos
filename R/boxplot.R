3df6f7a5e07ef729919118360cabf3d5d045c3cd
rakiduam <spartayotros@gmail.com>
2018-08-09 16:19:39 -0400

    Paleta de color brewer

--- R/boxplot.R
+++ R/boxplot.R
@@ -1,13 +1,4 @@
-
-source('color.R') # paletas de colores previamente elegidas
-
-plot(5, 
-     5, 
-     type="n", 
-     axes=FALSE, 
-     ann=FALSE, 
-     xlim=c(0, 10), 
-     ylim = c(0,10))
+source('color.R')
 
 # Codigo base para grafico tipo boxplot
 plot(1:5, col=colores[4])
